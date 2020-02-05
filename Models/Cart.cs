c6d96b660d692c13972a0a7a7acfe88465ea07e2
Agung Januardi <agung.januardi@bsi.co.id>
2020-02-05 15:46:01 +0700

    test

---
+++ Models/Cart.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace MVC.Models
+{
+    public class Cart
+    {
+        public int id {get;set;}
+        public int jumlahItem {get; set;}
+        public double jumlah {get; set;}
+    }
+}
